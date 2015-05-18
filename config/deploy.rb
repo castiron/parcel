5d779126e098160b19c3ceae8fbf1cacb0491157
Clark Burns <clark@castironcoding.com>
2015-05-18 15:15:45 -0700

    [FEATURE] Adds command to generate package list;

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :repo_url, 'git@github.com:castiron/parcel.git'
 # Hardcodes branch to always be master
 # This could be overridden in a stage config file
 
-set :deploy_to, -> { "#{fetch(:application)}" }
+set :application_root, '/home/parcel/parcel'
+set :deploy_to, -> { "#{fetch(:application_root)}" }
 
 set :log_level, :info
 
@@ -18,7 +19,8 @@ namespace :deploy do
   desc 'Post deploy script (currently just updates custom colors for all sites)'
   task :post_deployment do
     on roles(:app), in: :sequence, wait: 5 do
-    execute "bash #{fetch(:application)}/current/debs/setup.sh"
+    execute "bash #{fetch(:application_root)}/current/debs/setup.sh"
+    execute "cd #{fetch:application_root}/current/debs/ && dpkg-scanpackages amd64 | gzip -9c > amd64/Packages.gz"
   end
 end
 end
